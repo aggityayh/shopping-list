9f6c543f239f32dbd44120e8c0211c47611ffbbf
aggityayh <agitbarat@gmail.com>
2023-11-15 16:25:55 +0700

    Tutorial 8 done

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from main.views import show_main, create_product, show_xml, show_json
 from main.views import register #register merupakan nama fungsi dari views.py
 from main.views import login_user, logout_user #login_user merupakan nama fungsi dari views.py
 from main.views import edit_product, delete_product
-from main.views import get_product_json, add_product_ajax
+from main.views import get_product_json, add_product_ajax, create_product_flutter
 
 app_name = 'main'
 
@@ -19,4 +19,5 @@ urlpatterns = [
     path('delete/<int:id>', delete_product, name='delete_product'), # sesuaikan dengan nama fungsi yang dibuat
     path('get-product/', get_product_json, name='get_product_json'),
     path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
